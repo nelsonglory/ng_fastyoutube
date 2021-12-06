a98911952e155d81a548bed32997fa99186b97b8
Roderick Braun <roderick.braun@ph-freiburg.de>
2021-12-06 15:06:40 +0100

    Version compatible with 10.4 LTS

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,11 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+// plugin signature: <extension key without underscores> '_' <plugin name in lowercase>
+$pluginSignature = 'ngfastyoutube_pi';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    $pluginSignature,
+    // Flexform configuration schema file
+    'FILE:EXT:ng_fastyoutube/Configuration/FlexForms/flexform_pi.xml'
+    );
