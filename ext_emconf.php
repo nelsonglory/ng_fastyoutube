98e4a443d6436e6cb9bf97ab0cb72a37db53a063
System user; apache <roderick.braun@ph-freiburg.de>
2025-03-13 18:30:26 +0100

    Dropped TYPO3 10.4 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.6.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '10.4.0-12.4.99',
+			'typo3' => '11.5.0-12.4.99',
 		),
 		'conflicts' => array(
 		),
