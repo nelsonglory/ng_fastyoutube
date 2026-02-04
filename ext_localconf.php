70c91082d76ad5a9cb75f16430ba94fedef029e6
System user; apache <roderick.braun@ph-freiburg.de>
2026-02-04 16:52:29 +0100

    removed old typoscript

--- ext_localconf.php
+++ ext_localconf.php
@@ -19,26 +19,3 @@ ExtensionUtility::configurePlugin(
     ],
     ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
     );
-
-
-/*
-// wizards
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    'mod {
-            wizards.newContentElement.wizardItems.plugins {
-                elements {
-                    pi {
-                        iconIdentifier = ng_fastyoutube-svg-icon
-                        title = LLL:EXT:ng_fastyoutube/Resources/Private/Language/locallang.xlf:tx_ng_fastyoutube_pi.name
-                        description = LLL:EXT:ng_fastyoutube/Resources/Private/Language/locallang.xlf:tx_ng_fastyoutube_pi.description
-                        tt_content_defValues {
-                            CType = ngfastyoutube_pi
-                            list_type = ngfastyoutube_pi
-                        }
-                    }
-                }
-                show = *
-            }
-       }'
-    );
-*/
